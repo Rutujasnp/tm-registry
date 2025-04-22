5e8639a7d0e4b6c530398c9ef4acf038386eefed
Rutujasnp <rutujasnp@gmail.com>
2025-04-22 17:03:36 +0530

    initial commit

---
+++ src/main/java/com/hc/registry/TmRegistryApplication.java
@@ -0,0 +1,13 @@
+package com.hc.registry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TmRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TmRegistryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hc/registry/TmRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hc.registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TmRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
